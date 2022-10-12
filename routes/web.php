41a4ce40018883982ec64754eb921ec45abcda02
rlusitha <rlusitha@gmail.com>
2022-10-12 10:57:57 +0530

    before assigning middleware

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\PrescriptionController;
 use App\Http\Controllers\QuotationController;
+use App\Mail\QuotationEmail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,9 +26,19 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::view('/welcome1', 'welcome1');
 Route::view('/getQuotation', 'quotation');
-Route::resource('prescription', PrescriptionController::class);
+Route::resource('prescription', PrescriptionController::class)->middleware('auth');
 Route::resource('quotation', QuotationController::class);
 
 Route::get('view_prescriptions', [QuotationController::class, 'view_prescriptions'])->name('view_prescriptions');
 Route::get('create_quotation_view', [QuotationController::class, 'create_quotation_view']);
 Route::get('pdf', [QuotationController::class, 'quotation_pdf_generator'])->name('pdf');
+
+Route::get('send_email', function() {
+    $mailData = [
+        'name' => 'Lusitha',
+        'dob' => '20/12/1988'
+    ];
+
+    Mail::to("rlusitha@gmail.com")->send(new QuotationEmail($mailData));
+    dd('Mail Sent!');
+});
